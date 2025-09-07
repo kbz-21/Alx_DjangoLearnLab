219c29ea3b51ff1a7dfb636dcc99be4e0ef54f16
Kaleab Zewdie <kalzed1921@gmail.com>
2025-09-08 00:00:28 +0300

    Fix template paths to relationship_app/list_books.html and library_detail.html

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 from .models import Book
-from django.http import HttpResponse
 
-# Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
 
