727a73e31185756bc5bbe34ded79c6ab0850349e
Kaleab Zewdie <kalzed1921@gmail.com>
2025-09-14 08:57:58 -0700

    Add FBV and CBV views with URL routing and templates

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookshelf',  # new app added
+    'relationship_app',
 ]
 
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,8 +6,6 @@ def list_books(request):
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
-
-
 from django.views.generic.detail import DetailView
 from .models import Library
 
@@ -15,4 +13,30 @@ from .models import Library
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'  # <--- corrected path
+    context_object_name = 'library'
+
+
+
+
+# .....................................
+
+from django.shortcuts import render
+from .models import Book
+from django.http import HttpResponse
+
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
+
+
+# .....................................
+
+from django.views.generic.detail import DetailView
+from .models import Library
+
+# Class-based view to show library details and its books
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
     context_object_name = 'library'
